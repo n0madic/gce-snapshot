bd5aeb0ab279e989465df0ec7183efe931dfe730
Alexander Sheiko <alexander.sheiko@mgid.com>
2019-09-17 16:38:54 +0300

    Add region

--- cloud-function/deploy.sh
+++ cloud-function/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 gcloud functions deploy google-compute-snapshot \
+                        --region europe-west1 \
                         --runtime go111 \
                         --entry-point SnapshotPubSub \
                         --trigger-topic snapshots
